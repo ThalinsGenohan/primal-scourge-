0c7e9f6b5b637556969bb574b7a3912c5245544b
Thalins Genohan <thalinsgenohan@gmail.com>
2018-11-26 08:19:10 -0500

    quice more

--- src/Message.h
+++ src/Message.h
@@ -1,6 +1,7 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
+#include <ostream>
 #include <string>
 
 #include "User.h"
--- src/User.h
+++ src/User.h
@@ -1,6 +1,7 @@
 #ifndef USER_H
 #define USER_H
 
+#include <ostream>
 #include <string>
 #include <SFML/Graphics/Color.hpp>
 
