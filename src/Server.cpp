f27bc9830d0053a886ec92d279fc8f4afa03cf2c
Thalins Genohan <thalinsgenohan@gmail.com>
2018-12-07 12:47:05 -0500

    Update Server.cpp

--- src/Server.cpp
+++ src/Server.cpp
@@ -171,7 +171,7 @@ char Server::parseMessage(Message msg)
     if (s == "roll")
     {
       auto dice = msg.getMessage().substr(msg.getMessage().find(' '));
-      std::stringstream ss(dice.substr(6, dice.find('d')));
+      std::stringstream ss(dice.substr(0, dice.find('d')));
       auto diceNum = 0;
       ss >> diceNum;
       ss.clear();
